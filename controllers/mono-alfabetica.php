6a8eb88f304ee6db34ad98638d2ec6c8435550bb
car2854 <39568391+car2854@users.noreply.github.com>
2021-07-06 20:39:57 -0400

    Agregando toques

--- controllers/mono-alfabetica.php
+++ controllers/mono-alfabetica.php
@@ -5,8 +5,39 @@
   $n = strtoupper($n);
   $n = str_replace(' ','', $n);
 
-  echo "xD";
+  echo sustitucion_modo_alfabetica($n);
   
+
+  function sustitucion_modo_alfabetica($mensaje){
+    
+    $alfabeto = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+    $cantidad = strlen($mensaje);
+    $cantidadAlfabeto = count($alfabeto);
+
+    $nuevoTexto = "";
+
+    for ($i=0;$i<$cantidad;$i++){
+
+      $index = find_index($mensaje[$i],$alfabeto);
+
+      $nuevoTexto = $nuevoTexto . $alfabeto[($cantidadAlfabeto - 1) - $index];
+
+    }
+
+    return $nuevoTexto;
+
+  }
+
+  function find_index($value, $alfabeto){
+
+    for ($i=0;$i<count($alfabeto);$i++){
+  
+      if ($value == $alfabeto[$i]) return $i;
+  
+    }
+  
+    return -1;
+  }
 
 
 ?>

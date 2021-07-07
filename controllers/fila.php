b70a59e9ef675c2000bd6be2ed1795ba9f084c47
car2854 <39568391+car2854@users.noreply.github.com>
2021-07-06 20:26:03 -0400

    Terminado la migracion

--- controllers/fila.php
+++ controllers/fila.php
@@ -6,8 +6,47 @@
   $n = strtoupper($n);
   $n = str_replace(' ','', $n);
 
-  echo "xD";
+  echo cifrado_transposcion_por_filas($n, 1, $clave);
   
+  function cifrado_transposcion_por_filas($texto_en_claro, $cantidad_de_columnas, $caracter_para_rellenar){
+    $texto_en_claro = formatear_texto($texto_en_claro, $cantidad_de_columnas, $caracter_para_rellenar);
+    $texto_cifrado = "";    
+    $matriz =  str_split($texto_en_claro, $cantidad_de_columnas);
+    for($i = 0 ; $i < $cantidad_de_columnas; $i++){
+        for($j = 0 ; $j < sizeof($matriz) ; $j++){
+            $bloque = $matriz[$j];
+            $texto_cifrado = $texto_cifrado . $bloque[$i];                                 
+        }
+
+    }
+ 
+    return $texto_cifrado;
+}
+
+//PASAR TEXTO A MAYUSCULAS, QUITAR ESPACIOS, Y RELLENAR LOS CARACTERES FALTANTES
+function formatear_texto($texto_en_claro, $cantidad_de_columnas, $caracter_para_rellenar){
+  $texto_en_claro = strtoupper($texto_en_claro);
+  $texto_en_claro = quitar_espacios_en_blanco($texto_en_claro);
+  $relleno = strlen($texto_en_claro) % $cantidad_de_columnas;
+  if($relleno > 0){
+      $texto_en_claro = rellenar_texto($texto_en_claro,$cantidad_de_columnas-$relleno,$caracter_para_rellenar);
+  }
+
+  return $texto_en_claro;
+}
+
+function quitar_espacios_en_blanco($texto){
+  $longitud_del_texto = strlen($texto);
+  $nuevo_texto = "";
+
+  for($i = 0 ; $i < $longitud_del_texto ; $i++){
+      if($texto[$i] != " "){
+          $nuevo_texto = $nuevo_texto . $texto[$i];
+      }
+  }
+
+  return $nuevo_texto;
+}
 
 
 ?>

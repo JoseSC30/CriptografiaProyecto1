4f8d60202dc0f3dd456bfa732dbdd8efd340a5b3
car2854 <39568391+car2854@users.noreply.github.com>
2021-07-12 20:27:31 -0400

    Arreglando

--- controllers/poli-alfabetica.php
+++ controllers/poli-alfabetica.php
@@ -14,17 +14,18 @@
     $clave = str_replace(' ','', $clave);
   
     $cantidad = strlen($mensaje);
+    $cantidadClave = strlen($clave);
   
-    $alfabeto = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+    $alfabeto = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z");
 
     $texto_nuevo = "";
-  
+   
     for ($i=0;$i<$cantidad;$i++){
   
         $index1 = find_index($mensaje[$i], $alfabeto);
-        $index2 = find_index($clave[$i % 3], $alfabeto);
+        $index2 = find_index($clave[$i % $cantidadClave], $alfabeto);
   
-        $index = ($index1 + $index2 > 25)? ($index1 + $index2) - 26: $index1 + $index2;
+        $index = ($index1 + $index2 > 26)? ($index1 + $index2) - 27: $index1 + $index2;
   
         $texto_nuevo = $texto_nuevo . $alfabeto[$index];
   
